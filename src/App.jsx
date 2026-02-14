cc953e990a85cf7aa7066f8c23db1d4551b0d393
sarra mardessi <sarramardessi1@gmail.com>
2026-02-14 19:03:15 +0100

    Introduce student data and greeting in App component
    Added student information and a greeting function.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const studentname = "sarrah"
+  const student = {
+    name: "Sarra",
+    age: 20,
+    track: "Frontend"
+  };
+  function sayhello(){
+    
+       return `Hello, ${sarrah}!`
+  return (
+    <div>
+      <h1>{sayHello()}</h1>
+      <p>Welcome to React Basics, {sarrah}!</p>
+      <label htmlFor="nameInput">Enter your name:</label>
+      <input type="text" id="nameInput" />
+    </div>
+  )
+}
+  export default App;
+  /*
+Reflection Questions:
+1. One thing I understand well: How to render variables and functions in JSX.
+2. One thing still confusing: When to define variables inside vs outside components.
+3. One mistake I made and fixed: Tried rendering {student} directly and got an error.
+*/
+
+    import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -33,3 +65,29 @@ function App() {
 }
 
 export default App
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+
